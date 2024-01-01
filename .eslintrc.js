36aa9ba557d770fd8a9ab1da5cc98f54741bf08a
vincent.xiao <vincentchris@126.com>
2024-01-01 10:11:42 +0800

    feat: 添加后端app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,32 @@
 module.exports = {
-  extends: ['eslint-config-semistandard', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'tsconfig.json',
+    '**.md',
+    'tsconfig.**.json',
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'eslint-config-semistandard',
+    'prettier',
+  ],
 };
